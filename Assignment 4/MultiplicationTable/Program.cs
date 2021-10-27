4af3f4e29f2901226dca215e0f3ebc9fbad82ab6
behnamasaei <behnamasaei@gmail.com>
2021-10-27 22:00:55 +0330

    MultiplicationTable finished

---
+++ Assignment 4/MultiplicationTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(i*j+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
