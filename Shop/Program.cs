1c6d3000b78d21dbd112812189874c8ff0401125
behnamasaei <behnamasaei@gmail.com>
2021-11-03 10:23:43 +0330

    Create solution

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -6,7 +6,7 @@ namespace Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
