088241b59489feba60fc7ccecb26d982d34ccd94
behnamasaei <behnamasaei@gmail.com>
2021-11-17 19:07:49 +0330

    Publish project

---
+++ Assignment 7/Assignment 7/Marid/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var boys = new List<string>
+    { "ali", "reza", "yasin", "benyamin", "mehrdad", "sajjad", "aidin", "shahin"};
+
+var girls = new List<string>
+    { "sara", "zari", "neda", "homa", "eli", "goli", "mary", "mina"};
+
+var random = new Random();
+do
+{
+    var boy = boys[random.Next(boys.Count)];
+    var girl = girls[random.Next(girls.Count)];
+    Console.WriteLine($"{boy} with {girl}");
+    boys.Remove(boy);
+    girls.Remove(girl);
+} while (boys.Count>0);
---
+++ Assignment 7/Assignment 7/OOP-fraction/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class fraction
+{
+    public dynamic Sum(dynamic firstValue, dynamic secondValue)
+        => firstValue + secondValue;
+
+    public dynamic Mulit(dynamic firstValue, dynamic secondValue)
+        => firstValue * secondValue;
+
+    public dynamic Division(dynamic firstValue, dynamic secondValue)
+        => firstValue / secondValue;
+
+    public dynamic Submission(dynamic firstValue, dynamic secondValue)
+        => firstValue - secondValue;
+}
